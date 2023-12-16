248262775eafc119898b76bee65b7c06395894b1
Bellorico323 <mabo.orico@gmail.com>
2023-12-16 08:58:07 -0300

    Creating global styles and default theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+
+  'white-bg': '#FAFAFA',
+
+  'gray-100': '#F3F2F2',
+  'gray-200': '#EDEDED',
+  'gray-300': '#E6E5E5',
+  'gray-400': '#D7D5D5',
+
+  'brown-200': '#8D8686',
+  'brown-500': '#574F4D',
+  'brown-700': '#403937',
+  'brown-900': '#272221',
+
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-800': '#4B2995',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-500': '#DBAC2C',
+  'yellow-800': '#C47F17',
+}
