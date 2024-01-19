8bfe945f78e27872c7b80e8048ad1258c9648ae0
Bellorico323 <mabo.orico@gmail.com>
2024-01-19 16:01:35 -0300

    Adding actions for the reducer

--- src/reducers/actions.ts
+++ src/reducers/actions.ts
@@ -33,3 +33,52 @@ export type Actions =
         callback: NavigateFunction
       }
     }
+
+export function addItemAction(item: Item) {
+  return {
+    type: ActionTypes.ADD_ITEM,
+    payload: {
+      item,
+    },
+  } satisfies Actions
+}
+
+export function removeItemAction(itemId: Item['id']) {
+  return {
+    type: ActionTypes.REMOVE_ITEM,
+    payload: {
+      itemId,
+    },
+  } satisfies Actions
+}
+
+export function incrementItemQuantityAction(itemId: Item['id']) {
+  return {
+    type: ActionTypes.INCREMENT_ITEM_QUANTITY,
+    payload: {
+      itemId,
+    },
+  } satisfies Actions
+}
+
+export function decrementItemQuantityAction(itemId: Item['id']) {
+  return {
+    type: ActionTypes.DECREMENT_ITEM_QUANTITY,
+    payload: {
+      itemId,
+    },
+  } satisfies Actions
+}
+
+export function checkoutCartAction(
+  order: OrderFormData,
+  callback: NavigateFunction,
+) {
+  return {
+    type: ActionTypes.CHECKOUT_CART,
+    payload: {
+      order,
+      callback,
+    },
+  } satisfies Actions
+}
